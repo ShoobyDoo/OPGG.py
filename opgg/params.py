28835a1ae31e9c9e968d0107699bb1ddcb0e87d6
Shoaib Ali <coolbrampton@gmail.com>
2023-07-11 20:09:46 -0400

    add params struct for easily passing valid method params

---
+++ opgg/params.py
@@ -0,0 +1,27 @@
+# Quick and simple scraper to pull some data from OPGG using multisearch
+
+# Author  : Doomlad
+# Date    : 2023-07-05
+# Edit    : 2023-07-09
+# License : BSD-3-Clause
+
+
+class Regions:
+    NA = "NA"
+    EUW = "EUW"
+    EUNE = "EUNE"
+    KR = "KR"
+    JP = "JP"
+    BR = "BR"
+    LAN = "LAN"
+    LAS = "LAS"
+    OCE = "OCE"
+    RU = "RU"
+    TR = "TR"
+
+class By:
+    ID = "id"
+    KEY = "key"
+    NAME = "name"
+    COST = "cost"
+    
