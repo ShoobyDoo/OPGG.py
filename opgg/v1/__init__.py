6d7347bd6dd693b89f553cf6891864b1f55d02fa
Shoaib Ali <coolbrampton@gmail.com>
2025-11-17 02:50:57 -0500

    chore: remove old v1 source code

--- opgg/v1/__init__.py
+++
@@ -1,6 +0,0 @@
-# from opgg.summoner import Summoner, Game
-# from opgg.season import Season, SeasonInfo
-# from opgg.champion import ChampionStats, Champion, Spell, Passive, Skin, Price
-# from opgg.league_stats import LeagueStats, Tier, QueueInfo
-# from opgg.params import Region, By
-# from opgg.cacher import Cacher
